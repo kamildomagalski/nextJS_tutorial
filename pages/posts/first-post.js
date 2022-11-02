b9a5d9758604aeddd1f3c15a74e24f32a81c91e2
Kamil Domagalski <domagalski.kamil@gmail.com>
2022-11-02 12:52:12 +0100

    Add dynamic routes using getStaticPaths, add remark to read markdown, add styles

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import Head from "next/head";
 import Layout from "../../components/layout";
-
+// this file is not used in project, only for educational purposes
 export default function FirstPost() {
   return (
     <Layout>
